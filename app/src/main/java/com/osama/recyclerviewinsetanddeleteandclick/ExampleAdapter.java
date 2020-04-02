d24f5191459e57123b32b73d906f611ca934c76d
OsamaAldawoody <osamaashraf1650@gmail.com>
2020-04-02 13:01:08 +0200

    initial commit

---
+++ app/src/main/java/com/osama/recyclerviewinsetanddeleteandclick/ExampleAdapter.java
@@ -0,0 +1,87 @@
+package com.osama.recyclerviewinsetanddeleteandclick;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class ExampleAdapter extends RecyclerView.Adapter<ExampleAdapter.ExampleViewHolder> {
+    private ArrayList<ExampleItem> mExampleList;
+    private OnItemClickListener mListener;
+
+    public interface OnItemClickListener{
+        void onItemClick(int position);
+        void onItemDelete(int position);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener){
+        mListener = listener;
+    }
+    public ExampleAdapter(ArrayList<ExampleItem> exampleList) {
+        mExampleList = exampleList;
+    }
+
+    @Override
+    public ExampleViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.example_item, parent, false);
+        return new ExampleViewHolder(v,mListener);
+    }
+
+    @Override
+    public void onBindViewHolder(ExampleViewHolder holder, int position) {
+        ExampleItem currentItem = mExampleList.get(position);
+
+        holder.mImageView.setImageResource(currentItem.getImageResource());
+        holder.mTextView1.setText(currentItem.getText1());
+        holder.mTextView2.setText(currentItem.getText2());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mExampleList.size();
+    }
+
+    public static class ExampleViewHolder extends RecyclerView.ViewHolder {
+        public ImageView mImageView;
+        public ImageView mDeleteImageView;
+        public TextView mTextView1;
+        public TextView mTextView2;
+
+        public ExampleViewHolder(View itemView, final OnItemClickListener listener) {
+            super(itemView);
+            mImageView = itemView.findViewById(R.id.imageView);
+            mDeleteImageView = itemView.findViewById(R.id.delete_ic);
+            mTextView1 = itemView.findViewById(R.id.textView);
+            mTextView2 = itemView.findViewById(R.id.textView2);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (listener != null){
+                        int position = getAdapterPosition();
+                        if (position != RecyclerView.NO_POSITION){
+                            listener.onItemClick(position);
+                        }
+                    }
+                }
+            });
+            mDeleteImageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (listener != null){
+                        int position = getAdapterPosition();
+                        if (position != RecyclerView.NO_POSITION){
+                            listener.onItemDelete(position);
+                        }
+                    }
+                }
+            });
+        }
+    }
+}
